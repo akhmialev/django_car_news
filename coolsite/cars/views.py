c9bfb0b50b5b18d01acee2f0ba23f23d2ace181d
20390680 <20390680>
2022-04-25 17:12:44 +0300

    ready

--- coolsite/cars/views.py
+++ coolsite/cars/views.py
@@ -1,58 +1,142 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DeleteView, CreateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import *
 from .models import *
+from .utils import *
 
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная свзязь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'}
-]
 
-def index(request):
-    posts = Car.objects.all()
-    cats = Category.objects.all()
-    context = {
-        'posts': posts,
-        'cats': cats,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0
-    }
-    return render(request, 'cars/index.html', context=context)
+
+class CarHome(DataMixin, ListView):
+    model = Car
+    template_name = 'cars/index.html'
+    context_object_name = 'posts'
+
+
+    def get_context_data(self, *, objext_list=None, **kwargs ):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def get_queryset(self):
+        return Car.objects.filter(is_published=True).select_related('cat')
+
+
 
 def about(request):
     return render(request, 'cars/about.html', {'menu': menu, 'title': "О сайте"})
 
-def addpage(request):
-    return HttpResponse("add_page")
 
-def login(request):
-    return HttpResponse("login")
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'cars/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
 
-def contact(request):
-    return HttpResponse("contact")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление статьи')
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+
+
+# def login(request):
+#     return HttpResponse("login")
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'cars/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная свзять')
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
-def show_post(request,post_id):
-    return HttpResponse(f"show_post {post_id}")
 
-def show_category(request,cat_id):
-    posts = Car.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
-    if len(posts) == 0:
-        raise Http404()
+class ShowPost(DataMixin, DeleteView):
+    model = Car
+    template_name = 'cars/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    context = {
-        'posts': posts,
-        'cats': cats,
-        'menu': menu,
-        'title': 'Отоброжение по рубрикам',
-        'cat_selected': cat_id,
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
 
-    return render(request, 'cars/index.html', context=context)
+
+
+class CarCategory(DataMixin, ListView):
+    model = Car
+    template_name = 'cars/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name),
+                                      cat_selected=c.pk)
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+
+    def get_queryset(self):
+        return Car.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'cars/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'cars/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'captcha',
     'cars.apps.CarsConfig'
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'coolsite.urls'
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -127,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
